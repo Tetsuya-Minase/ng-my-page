2f6c447ee69641119ab778460f6eae8c3b3d804b
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2019-12-28 13:15:16 +0900

    feat: create top template.🐥

---
+++ src/app/page/top/top.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopComponent } from './component/top.component';
+
+
+
+@NgModule({
+  declarations: [TopComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopModule { }
