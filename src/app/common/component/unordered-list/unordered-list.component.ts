05f00291dae832ef6fd5db335c8f5695142bb4f5
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2019-12-31 01:50:17 +0900

    style: unordered list is line.🎨

--- src/app/common/component/unordered-list/unordered-list.component.ts
+++ src/app/common/component/unordered-list/unordered-list.component.ts
@@ -7,11 +7,12 @@ import { LinkListItem } from '../../model/ListItem';
     <ul>
       <li *ngFor="let item of itemList" class="ul-component">
         <a [href]="item.linkUrl" target="_blank" class="ul-component__link">
-          <img [src]="item.icon" [alt]="item.linkText" width="30" class="ul-component__link__icon">
+          <img [src]="item.icon" [alt]="item.linkText" width="64" height="64" class="ul-component__link__icon">
         </a>
       </li>
     </ul>
-  `
+  `,
+  styles: ['.ul-component { display: inline; }']
 })
 export class UnorderedListComponent {
   @Input() itemList: LinkListItem[];
