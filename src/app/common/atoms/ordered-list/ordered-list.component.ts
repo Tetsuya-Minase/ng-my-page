317a43deff49d9ba13b013fa8544c16c6eeedf91
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-27 23:57:05 +0900

    refactor: add html & css file.🥚

--- src/app/common/atoms/ordered-list/ordered-list.component.ts
+++ src/app/common/atoms/ordered-list/ordered-list.component.ts
@@ -3,16 +3,7 @@ import { LinkListItem } from '../../model/ListItem';
 
 @Component({
   selector: 'ol-component',
-  template: `
-    <ol>
-      <li *ngFor="let item of itemList">
-        <article>
-          <h1><a [href]="item.linkUrl" target="_blank">{{item.linkText}}</a></h1>
-          <p>{{item.description}}</p>
-        </article>
-      </li>
-    </ol>
-  `
+  templateUrl: './ordered-list.component.html'
 })
 export class OrderedListComponent {
   @Input() itemList: LinkListItem[];
--- src/app/common/atoms/unordered-list/unordered-list.component.ts
+++ src/app/common/atoms/unordered-list/unordered-list.component.ts
@@ -3,18 +3,7 @@ import { LinkListItem } from '../../model/ListItem';
 
 @Component({
   selector: 'ul-component',
-  template: `
-    <ul [ngClass]="listClass">
-      <li *ngFor="let item of itemList" [ngClass]="itemClass">
-        <a [href]="item.linkUrl" target="_blank" class="list__link">
-          <ng-container [ngSwitch]="type">
-            <img *ngSwitchCase="'icon'" [src]="item.icon" [alt]="item.linkText" [ngClass]="iconClass">
-            <ng-container *ngSwitchCase="'text'">{{item.linkText}}</ng-container>
-          </ng-container>
-        </a>
-      </li>
-    </ul>
-  `,
+  templateUrl: './unordered-list.component.html',
   styleUrls: ['./unordered-list.component.scss']
 })
 export class UnorderedListComponent implements OnInit {
